1dbb88b2fb24df2ebdd9844e2bdc008e5252bf87
agoodman1999 <agoodman@civiconnect.ca>
2023-05-26 08:49:58 -0400

    initial commit

---
+++ launchInstance.js
@@ -0,0 +1,40 @@
+require('dotenv').config()
+const { LightsailClient, CreateInstancesCommand, GetBlueprintsCommand, GetBundlesCommand } = require("@aws-sdk/client-lightsail")
+
+const client = new LightsailClient({
+	region: process.env.AWS_REGION,
+	credentials: {
+		accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+	}
+})
+
+async function printBlueprints() {
+	const blueprintsCommand = new GetBlueprintsCommand({ includeInactive: false })
+	const data = await client.send(blueprintsCommand)
+	console.log(data)
+}
+
+async function printBundles() {
+	const bundlesCommand = new GetBundlesCommand({ includeInactive: false })
+	const data = await client.send(bundlesCommand)
+	console.log(data)
+}
+
+async function createInstance(instanceName) {
+	const createInstanceCommand = new CreateInstancesCommand({
+		availabilityZone: process.env.AVAILABILITY_ZONE,
+		blueprintId: process.env.BLUEPRINT_ID,
+		bundleId: process.env.BUNDLE_ID,
+		//userData: "string",
+		//keyPairName: "string",
+		instanceNames: [instanceName]
+	})
+
+	const data = await client.send(createInstanceCommand)
+	console.log(data)
+}
+
+//createInstance("austin-auto-deploy-test-instance")
+printBlueprints()
+
