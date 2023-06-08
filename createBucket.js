32fa4e330ffc59895a3983333139e5785963b23c
agoodman1999 <agoodman@civiconnect.ca>
2023-06-08 07:09:49 -0400

    implement createBucket

--- createBucket.js
+++ createBucket.js
@@ -1,10 +1,44 @@
 require('dotenv').config()
-const { S3Client } = require("@aws-sdk/client-s3")
+const { S3Client, CreateBucketCommand, PutPublicAccessBlockCommand } = require("@aws-sdk/client-s3")
 
-const s3Client = new S3Client({
+const client = new S3Client({
 	region: process.env.AWS_REGION,
 	credentials: {
 		accessKeyId: process.env.AWS_ACCESS_KEY_ID,
 		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 	}
 })
+
+async function createBucket(bucketName) {
+	const createBucketCommand = new CreateBucketCommand({
+		Bucket: bucketName,
+		ACL: "bucket-owner-full-control",
+		ObjectOwnership: "BucketOwnerPreferred"
+	})
+
+	const createBucketData = await client.send(createBucketCommand)
+	console.log(createBucketData)
+}
+
+async function putPublicAccessBlock(bucketName) {
+	const putPublicAccessBlockCommand = new PutPublicAccessBlockCommand({
+		Bucket: bucketName,
+		PublicAccessBlockConfiguration: {
+			BlockPublicAcls: false,
+			BlockPublicPolicy: true,
+			IgnorePublicAcls: false,
+			RestrictPublicBuckets: true
+		}
+	})
+
+	const putPublicAccessBlockData = await client.send(putPublicAccessBlockCommand)
+	console.log(putPublicAccessBlockData)
+}
+
+async function main() {
+	await createBucket(process.env.S3_BUCKET_NAME)
+	await S3Client.waitForBucketExists({ Bucket: process.env.S3_BUCKET_NAME })
+	await putPublicAccessBlock(process.env.S3_BUCKET_NAME)
+}
+
+main()
