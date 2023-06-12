c7c02ae346d201f9e176c2aa9e730b1f6bdd82a3
agoodman1999 <agoodman@civiconnect.ca>
2023-06-12 12:41:20 -0400

    accept existing_resource_behaviour as an input

--- createBucket.js
+++ createBucket.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const { S3Client, CreateBucketCommand, PutPublicAccessBlockCommand, waitUntilBucketExists } = require("@aws-sdk/client-s3")
+const { S3Client, CreateBucketCommand, PutPublicAccessBlockCommand, waitUntilBucketExists, HeadBucketCommand, DeleteBucketCommand } = require("@aws-sdk/client-s3")
 
 const client = new S3Client({
 	region: process.env.AWS_REGION,
@@ -17,7 +17,7 @@ async function createBucket(bucketName) {
 	})
 
 	const createBucketData = await client.send(createBucketCommand)
-	console.log(createBucketData)
+	return createBucketData
 }
 
 async function putPublicAccessBlock(bucketName) {
@@ -32,13 +32,58 @@ async function putPublicAccessBlock(bucketName) {
 	})
 
 	const putPublicAccessBlockData = await client.send(putPublicAccessBlockCommand)
-	console.log(putPublicAccessBlockData)
+	return putPublicAccessBlockData
+}
+
+async function doesBucketExist(bucketName) {
+	const headBucketCommand = new HeadBucketCommand({ Bucket: bucketName })
+	let bucketExists = false
+	try {
+		const headBucketData = await client.send(headBucketCommand)
+		bucketExists = true
+	} catch (err) {
+		bucketExists = false
+	}
+	return bucketExists
+}
+
+async function deleteBucket(bucketName) {
+	const deleteBucketCommand = new DeleteBucketCommand({ Bucket: bucketName })
+	const deleteBucketData = await client.send(deleteBucketCommand)
+	return deleteBucketData
 }
 
 async function main() {
-	await createBucket(process.env.S3_BUCKET_NAME)
-	await waitUntilBucketExists({ client, maxWaitTime: 60, minDelay: 1 }, { Bucket: process.env.S3_BUCKET_NAME })
-	await putPublicAccessBlock(process.env.S3_BUCKET_NAME)
+	//check if bucket exists
+	console.log("checking if bucket exists...")
+	let bucketExists = await doesBucketExist(process.env.S3_BUCKET_NAME)
+
+	if (process.env.EXISTING_RESOURCE_BEHAVIOR == "fail" && bucketExists) {
+		throw new Error(`Bucket ${process.env.S3_BUCKET_NAME} already exists`)
+	}
+
+	if (process.env.EXISTING_RESOURCE_BEHAVIOR == "replace" && bucketExists) {
+		await deleteBucket(process.env.S3_BUCKET_NAME)
+
+		//wait until bucket is deleted
+		console.log("deleting existing bucket...")
+		let _bucketExists = true
+		while (_bucketExists) {
+			_bucketExists = await doesBucketExist(process.env.S3_BUCKET_NAME)
+			await new Promise(resolve => setTimeout(resolve, 1000))
+		}
+		bucketExists = false
+	}
+
+	if (!bucketExists) {
+		console.log("creating bucket...")
+		await createBucket(process.env.S3_BUCKET_NAME)
+		await waitUntilBucketExists({ client, maxWaitTime: 60, minDelay: 1 }, { Bucket: process.env.S3_BUCKET_NAME })
+		await putPublicAccessBlock(process.env.S3_BUCKET_NAME)
+		console.log("bucket created successfully")
+	} else {
+		console.log("using existing bucket")
+	}
 }
 
 main()
--- launchInstance.js
+++ launchInstance.js
@@ -1,6 +1,11 @@
 const fs = require('fs')
 require('dotenv').config()
-const { LightsailClient, CreateInstancesCommand, GetBlueprintsCommand, GetBundlesCommand, GetInstanceCommand, GetInstanceStateCommand, AllocateStaticIpCommand, AttachStaticIpCommand, GetStaticIpCommand } = require("@aws-sdk/client-lightsail")
+const {
+	LightsailClient, CreateInstancesCommand, GetBlueprintsCommand,
+	GetBundlesCommand, GetInstanceCommand, GetInstanceStateCommand,
+	AllocateStaticIpCommand, AttachStaticIpCommand, GetStaticIpCommand,
+	DeleteInstanceCommand, DeleteStaticIpCommand
+} = require("@aws-sdk/client-lightsail")
 const { S3Client } = require("@aws-sdk/client-s3")
 
 const runningStateCheckTimeout = 600
@@ -47,6 +52,12 @@ async function createInstance(instanceName) {
 	const data = await client.send(createInstanceCommand)
 }
 
+async function deleteInstance(instanceName) {
+	const deleteInstanceCommand = new DeleteInstanceCommand({ instanceName: instanceName })
+	const data = await client.send(deleteInstanceCommand)
+	return data
+}
+
 async function getInstance(instanceName) {
 	const getInstanceCommand = new GetInstanceCommand({ instanceName: instanceName })
 	const data = await client.send(getInstanceCommand)
@@ -71,38 +82,111 @@ async function attachStaticIp(instanceName, staticIpName) {
 	return data
 }
 
+async function deleteStaticIp(staticIpName) {
+	const deleteStaticIpCommand = new DeleteStaticIpCommand({ staticIpName: staticIpName })
+	const data = await client.send(deleteStaticIpCommand)
+	return data
+}
+
 async function getStaticIp(staticIpName) {
 	const getStaticIpCommand = new GetStaticIpCommand({ staticIpName: staticIpName })
 	const data = await client.send(getStaticIpCommand)
 	return data.staticIp.ipAddress
 }
 
-async function main() {
-	//create instance
-	console.log("creating instance")
-	await createInstance(process.env.INSTANCE_NAME)
-
-	//wait for instance to be running
+async function waitForRunningState(instanceName) {
 	console.log("waiting for instance to be running")
-	let state = await getInstanceState(process.env.INSTANCE_NAME)
+	let state = await getInstanceState(instanceName)
 	let startTime = Date.now()
 	while ((!state || state.name != "running") && Date.now() - startTime < runningStateCheckTimeout * 1000) {
 		await new Promise(resolve => setTimeout(resolve, runningStateCheckInterval * 1000))
-		state = await getInstanceState(process.env.INSTANCE_NAME)
+		state = await getInstanceState(instanceName)
 		console.log("instance state is " + (state.name || "undefined"))
 	}
+}
+
+async function main() {
+	//first check if an instance with the same name already exists
+	console.log("checking if instance already exists")
+	let instance
+	try {
+		instance = await getInstance(process.env.INSTANCE_NAME)
+	} catch (err) { }
+
+	//if instance exists and EXISTING_RESOURCE_BEHAVIOR is "fail", fail the action
+	if (process.env.EXISTING_RESOURCE_BEHAVIOR == "fail" && instance) {
+		throw new Error("instance with name " + process.env.INSTANCE_NAME + " already exists")
+	}
+
+	//if instance exists and EXISTING_RESOURCE_BEHAVIOR is "replace", delete the instance
+	if (process.env.EXISTING_RESOURCE_BEHAVIOR == "replace" && instance) {
+		console.log("deleting existing instance...")
+		await deleteInstance(process.env.INSTANCE_NAME)
+
+		//wait for instance to be deleted
+		let instanceDeleted = false
+		while (!instanceDeleted) {
+			try {
+				instance = await getInstance(process.env.INSTANCE_NAME)
+			} catch (err) {
+				instanceDeleted = true
+			}
+			await new Promise(resolve => setTimeout(resolve, 1000))
+		}
+		instance=undefined
+		console.log("instance deleted")
+	}
+
+	//only create a new instance if one doesn't already exist
+	if (!instance) {
+		console.log("creating instance")
+		await createInstance(process.env.INSTANCE_NAME)
+	}
+
+	//wait for instance to be running
+	await waitForRunningState(process.env.INSTANCE_NAME)
 
 	//get instance public ip
-	const instance = await getInstance(process.env.INSTANCE_NAME)
+	instance = await getInstance(process.env.INSTANCE_NAME)
 	let publicIp = instance.instance.publicIpAddress
 
 	//create static ip
-	const createStaticIP = process.env.CREATE_STATIC_IP
-	if (createStaticIP == "true") {
+	if (process.env.CREATE_STATIC_IP == "true") {
+		//first check if a static ip with the same name already exists
+		console.log("checking if static ip already exists...")
+		let staticIp
+		try {
+			staticIp = await getStaticIp(`${process.env.INSTANCE_NAME}-static-ip`)
+		} catch (err) { }
+
+		//if static ip exists and EXISTING_RESOURCE_BEHAVIOR is "fail", fail the action
+		if (process.env.EXISTING_RESOURCE_BEHAVIOR == "fail" && staticIp) {
+			throw new Error("static ip with name " + process.env.INSTANCE_NAME + "-static-ip already exists")
+		}
+
+		//if static ip exists and EXISTING_RESOURCE_BEHAVIOR is "replace", delete the static ip
+		if (process.env.EXISTING_RESOURCE_BEHAVIOR == "replace" && staticIp) {
+			console.log("deleting existing static ip...")
+			await deleteStaticIp(`${process.env.INSTANCE_NAME}-static-ip`)
+
+			//wait for static ip to be deleted
+			let staticIpDeleted = false
+			while (!staticIpDeleted) {
+				try {
+					staticIp = await getStaticIp(`${process.env.INSTANCE_NAME}-static-ip`)
+				} catch (err) {
+					staticIpDeleted = true
+				}
+				await new Promise(resolve => setTimeout(resolve, 1000))
+			}
+			staticIp=undefined
+			console.log("static ip deleted")
+		}
+
 		console.log("creating static ip")
 		await allocateStaticIp()
 		await attachStaticIp(process.env.INSTANCE_NAME, `${process.env.INSTANCE_NAME}-static-ip`)
-		const staticIp = await getStaticIp(`${process.env.INSTANCE_NAME}-static-ip`)
+		staticIp = await getStaticIp(`${process.env.INSTANCE_NAME}-static-ip`)
 		publicIp = staticIp
 	}
 
