041dc74e37ad25f36ef99cdd85055d35660a47ac
agoodman1999 <agoodman@civiconnect.ca>
2023-06-15 12:54:14 -0400

    use absolute paths

--- ansible/files/backup-strapi.sh
+++ ansible/files/backup-strapi.sh
@@ -2,7 +2,7 @@
 instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id | cut -d '-' -f 2)
 file_name="$1-$instance_id"
 
-cd ~/strapi
+cd /home/bitnami/strapi
 npm run strapi export -- --no-encrypt --file ../"$file_name"
 aws s3 cp ../"$file_name".tar.gz s3://civiconnect-automated-strapi-backups/"$file_name".tar.gz
 rm ../"$file_name".tar.gz
